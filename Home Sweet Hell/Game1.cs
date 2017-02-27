9a4716311ef43ded86e459aa3c573889317f87e0
LAPTOP-CSIU6NUE\James <jimmays.jl@gmail.com>
2017-02-27 17:20:14 -0500

    Base code

---
+++ Home Sweet Hell/Game1.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Home_Sweet_Hell
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        // initializes map
+        Map level = new Map();
+
+        // Mouse states used to track Mouse button press
+        MouseState currentMouseState;
+        MouseState previousMouseState;
+
+        // texture hight and width dimensions
+        private int txtHeight = 32; // placeholder value
+        private int txtWidth = 32; // placeholder value
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // draws a window that is multiplied by texture dimensions to ensure that thw window is large enough
+            graphics.PreferredBackBufferWidth = level.Width * txtWidth;
+            graphics.PreferredBackBufferHeight = level.Height * txtHeight;
+            graphics.ApplyChanges();
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            // Get Mouse State then Capture the Button type and Respond Button Press
+            Vector2 mousePosition = new Vector2(currentMouseState.X, currentMouseState.Y);
+            previousMouseState = currentMouseState;
+            currentMouseState = Mouse.GetState();
+
+            // if mouse is clicked, check cooridnates
+            if (currentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                
+            }
+            
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Home Sweet Hell/Map.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Generic Map class
+namespace Home_Sweet_Hell
+{
+    class Map
+    {
+        // height and length of map array
+        private int width = 32; // placeholder value
+        private int height = 32; // placeholder value
+
+        // constructor
+        public Map()
+        {
+           
+        }
+
+        // properties for height and width
+        public int Width
+        {
+            get { return width; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+        }
+    }
+}
