f6544b011693a9a3a39acab9926ce2bfd0533a53
LAPTOP-ADS9BEPP\merca <nvm6501@rit.edu>
2017-04-19 09:32:26 -0400

    A small change

--- Home Sweet Hell/Enemy.cs
+++ Home Sweet Hell/Enemy.cs
@@ -152,6 +152,7 @@ namespace Home_Sweet_Hell
                                 {
                                     position = new Rectangle(new Point(next.Position.Y * 50, next.Position.X * 50), new Point(50, 50));
                                     obj.Walkable = false;
+                                    return;
                                 }
                             }
 
